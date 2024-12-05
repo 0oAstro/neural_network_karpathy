1696247e66b2ca1427606a418ea72b249c937a76
Shaurya Pratap Singh (Astro) <ee1240486@iitd.ac.in>
2024-12-06 03:22:34 +0530

    FIXING

--- kernels.nix
+++ kernels.nix
@@ -1,5 +1,11 @@
-{pkgs, ...}: {
+{...}: {
   kernel.python.minimal = {
     enable = true;
   };
+
+  kernel.python.nn.enable = true;
+  kernel.python.nn.extraPackages = ps: [
+    ps.numpy
+    ps.matplotlib
+  ];
 }
