435da7f465b05b8bb3b6309e0d531e43016479c2
Shaurya Pratap Singh (Astro) <ee1240486@iitd.ac.in>
2024-12-06 03:19:02 +0530

    FIXING

--- flake.nix
+++ flake.nix
@@ -1,32 +1,43 @@
 {
-  description = "A Nix-flake-based Jupyter development environment";
+  description = "Your jupyenv project";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  nixConfig.extra-substituters = [
+    "https://tweag-jupyter.cachix.org"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
+  ];
 
-  outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f {
-          pkgs = import nixpkgs { inherit system; };
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.jupyenv.url = "github:tweag/jupyenv";
+
+  outputs = {
+    self,
+    flake-compat,
+    flake-utils,
+    nixpkgs,
+    jupyenv,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+    ]
+    (
+      system: let
+        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
+        jupyterlab = mkJupyterlabNew ({...}: {
+          nixpkgs = inputs.nixpkgs;
+          imports = [(import ./kernels.nix)];
         });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          packages = with pkgs; [ poetry python311 ] ++ (with python311Packages; [
-            ipykernel
-            numpy
-            matplotlib
-            pip
-            venvShellHook
-          ]);
-        };
-      });
-    };
+      in rec {
+        packages = {inherit jupyterlab;};
+        packages.default = jupyterlab;
+        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
+        apps.default.type = "app";
+      }
+    );
 }
